fdc48b574b19e208864099c421d0133908885b07
Melissa Tammy <mel.tammy2@gmail.com>
2020-07-23 16:00:59 -0300

    usercontroller alterado para class

--- src/router/UserRouter.ts
+++ src/router/UserRouter.ts
@@ -1,20 +1,14 @@
 import express from 'express';
-import { 
-    invite, 
-    undo,
-    getFeedByType, 
-    getFeed,
-  getByType
-} from "../controller/UserController";
+import { UserController } from "../controller/UserController";
 
 export const userRouter = express.Router();
 
-userRouter.get("/feed/:type", getByType);
+userRouter.get("/feed/:type", new UserController().getByType);
 
-userRouter.post('/invite/:id', invite);
+userRouter.post('/invite/:id', new UserController().invite);
 
-userRouter.delete('/undo/:id', undo);
+userRouter.delete('/undo/:id', new UserController().undo);
 
-userRouter.get("/feed/:type", getFeedByType);
+userRouter.get("/feed/:type", new UserController().getFeedByType);
 
-userRouter.get("/feed", getFeed);
+userRouter.get("/feed", new UserController().getFeed);
